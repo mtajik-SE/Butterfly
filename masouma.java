465f5642e55735d98fe4d06ad8a16f62113c50cc
mtajik-SE <71802461+mtajik-SE@users.noreply.github.com>
2020-09-24 07:39:55 +0430

    change

--- masouma.java
+++ masouma.java
@@ -12,5 +12,6 @@ public class AddTwoIntegers {
         System.out.println("The sum is: " + sum);
         System.out.println("The sum is: " + sum);
         System.out.println("The sum is: " + sum);
+        System.out.println("The sum is: " + sum);
     }
 }
